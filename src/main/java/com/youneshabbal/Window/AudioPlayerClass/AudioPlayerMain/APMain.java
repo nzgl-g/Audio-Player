5da985ebf246c9a82b77f2e059770c5066bc85f0
Younes Habbal <103865507+Youneshabbal@users.noreply.github.com>
2023-06-16 22:11:45 +0100

    Add files via upload

---
+++ src/main/java/com/youneshabbal/Window/AudioPlayerClass/AudioPlayerMain/APMain.java
@@ -0,0 +1,12 @@
+package com.youneshabbal.Window.AudioPlayerClass.AudioPlayerMain;
+import com.youneshabbal.Window.AudioPlayerClass.APClass;
+import javax.swing.*;
+public class APMain {
+    public static void main(String[] args) {
+          try {
+                new APClass();
+          } catch (Exception e) {
+                e.printStackTrace();
+          }
+    }
+}
